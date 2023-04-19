928686f2be6d7e5f69b61205a77dc8a7b9abe6e2
Qara <qarahumbtov@gmail.com>
2023-04-19 23:21:31 +0400

    Add Navbar UI

---
+++ src/layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// Components
+import Navbar from '../containers/navbar/Navbar'
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Layout
